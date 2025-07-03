5efc84199fa48e8608e542e70c62dade76bc02cf
Stanislav Evstratenko <evstratenko.sv98@gmail.com>
2025-07-04 00:52:06 +0300

    version 0.3.0 optimization

--- pages/api/back/utils/getSites.ts
+++ pages/api/back/utils/getSites.ts
@@ -16,49 +16,62 @@ export const getSites = async (
     const {text, countryTitle} = params;
     const existingSites = await getExistingSites();
     const googleResponse = await searchPlaces(text, countryTitle);
-    const logPlaces: LogPlace[] = [];
 
     if(!googleResponse) {
         return null;
     }
 
-    for(const place of googleResponse.places) {
-        const logPlace: LogPlace = {
-            displayName: place.displayName,
-            formattedAddress: place.formattedAddress,
-            isWebsiteUri: !!place.websiteUri,
-            websiteUri: place.websiteUri,
-            isError: false,
-            error: '',
-            isLanding: false,
-            isSocialNetwork: false,
-            siteDetails: {},
-            isExistsInCheckList: false,
-            status: Status.UNSUITABLE,
-        }
+    const logPlaces: LogPlace[] = await Promise.all(
+        googleResponse.places.map(async (place) => {
+            const logPlace: LogPlace = {
+                displayName: place.displayName,
+                formattedAddress: place.formattedAddress,
+                isWebsiteUri: !!place.websiteUri,
+                websiteUri: place.websiteUri,
+                isError: false,
+                error: '',
+                isLanding: false,
+                isSocialNetwork: false,
+                siteDetails: {},
+                isExistsInCheckList: false,
+                status: Status.UNSUITABLE,
+            };
 
-        if (logPlace.isWebsiteUri) {
-            await checkIfLandingPage(place.websiteUri).then(result => {
-                if (result.error) {
+            if (logPlace.isWebsiteUri) {
+                try {
+                    const result = await checkIfLandingPage(place.websiteUri);
+                    if (result.error) {
+                        logPlace.isError = true;
+                        logPlace.error = `Ошибка: ${result.message}`;
+                    } else {
+                        logPlace.isLanding = result.isLanding;
+                        logPlace.siteDetails = {...result.details};
+                    }
+                } catch (e) {
                     logPlace.isError = true;
-                    logPlace.error = `Ошибка: ${result.message}`;
-                } else {
-                    logPlace.isLanding = result.isLanding;
-                    logPlace.siteDetails = {...result.details};
+                    logPlace.error = `Ошибка при проверке: ${e}`;
                 }
-            });
-            logPlace.isSocialNetwork = checkSocialNetwork(logPlace.websiteUri);
-        }
-        if(logPlace.isWebsiteUri && !logPlace.isLanding) {
-            const website = formatUrl(place.websiteUri);
-            logPlace.isExistsInCheckList = existingSites.includes(website);
-        }
-        if(logPlace.isWebsiteUri && !logPlace.isLanding && !logPlace.isExistsInCheckList && !logPlace.isError && !logPlace.isSocialNetwork) {
-            logPlace.status = Status.SUITABLE;
-        }
 
-        logPlaces.push(logPlace);
-    }
+                logPlace.isSocialNetwork = checkSocialNetwork(logPlace.websiteUri);
+
+                if (!logPlace.isLanding) {
+                    const website = formatUrl(place.websiteUri);
+                    logPlace.isExistsInCheckList = existingSites.includes(website);
+                }
+
+                if (
+                    !logPlace.isLanding &&
+                    !logPlace.isExistsInCheckList &&
+                    !logPlace.isError &&
+                    !logPlace.isSocialNetwork
+                ) {
+                    logPlace.status = Status.SUITABLE;
+                }
+            }
+
+            return logPlace;
+        })
+    );
 
     return {
         logPlaces,
